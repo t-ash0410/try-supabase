6257067a0a88d536ef1896a880b66bed06a20f96
t-ash0410 <t_ash0410@yahoo.co.jp>
2025-03-28 11:16:59 +0900

    add consts

---
+++ ts/web/packages/frontend/app/consts/page-paths.ts
@@ -0,0 +1,12 @@
+export const pagePaths = {
+  public: {
+    root: {
+      path: '/',
+      file: 'routes/public/root.tsx',
+    },
+    signin: {
+      path: '/signin',
+      file: 'routes/public/signin.tsx',
+    },
+  },
+} as const
