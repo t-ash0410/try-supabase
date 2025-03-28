fdc4d8d430550af8b39594d3dbbeebf6669325d2
t-ash0410 <t_ash0410@yahoo.co.jp>
2025-03-28 11:18:13 +0900

    add features

---
+++ ts/web/packages/frontend/app/features/signin/components/signin-form/signin.tsx
@@ -0,0 +1,9 @@
+export function SigninForm() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="flex w-full max-w-md flex-col items-center gap-16">
+        <img src="/images/logo.jpg" width={120} height={120} />
+      </div>
+    </div>
+  )
+}
